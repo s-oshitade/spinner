09ac1b28db81bb1778aa030a967cb10c7af6b194
Seun Oshitade <seun.oshitade@gmail.com>
2021-11-12 20:59:21 +0000

    Refactored animation with setTimeout

--- spinner2.js
+++ spinner2.js
@@ -1,8 +1,8 @@
-const array = ['\r|  ', '\r/  ', '\r-  ','\r\\  ', '\r|  ', '\r/  ', '\r-  ', '\r|  \n']
+const array = ['\r|  ', '\r/  ', '\r-  ','\r\\  ', '\r|  ', '\r/  ', '\r-  ', '\r|  \n'];
 for (let index = 0; index < array.length; index++) {
   const element = array[index];
   const num = index + 1;
   setTimeout(() => {
-  process.stdout.write(element);
+    process.stdout.write(element);
   }, 100 * num);
 }
