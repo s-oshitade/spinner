6cf4fa35da1842584cac17b09a31bdb2a4c2fe1d
Seun Oshitade <seun.oshitade@gmail.com>
2021-11-15 22:00:48 +0000

    Refactored!

--- spinner2.js
+++ spinner2.js
@@ -1,8 +1,20 @@
-const array = ['\r|  ', '\r/  ', '\r-  ','\r\\  ', '\r|  ', '\r/  ', '\r-  ', '\r|  \n'];
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-  const num = index + 1;
-  setTimeout(() => {
-    process.stdout.write(element);
-  }, 100 * num);
+const spinner = ['|  ', '/  ', '-  ','\\  ', '|  ', '/  ', '-  ', '|  \n'];
+const wait = 0
+for (const symbol of spinner) {
+  wait += 100;
+  setTimeout(() => process.stdout.write(`\r${symbol}`), wait);
 }
+
+
+
+
+
+
+// const array = ['\r|  ', '\r/  ', '\r-  ','\r\\  ', '\r|  ', '\r/  ', '\r-  ', '\r|  \n'];
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+//   const num = index + 1;
+//   setTimeout(() => {
+//     process.stdout.write(element);
+//   }, 100 * num);
+// }
